408fc4416528b6f21da017335cd96818d6d8d4a7
iamsky <akashprajapati224224@gmail.com>
2026-09-06 21:30:19 +0530

    Splitwise-LLD-basic-structure

---
+++ Splitwise/src/main/java/org/lld/models/Transaction.java
@@ -0,0 +1,30 @@
+package org.lld.models;
+
+public class Transaction {
+    private final User from;
+    private final User to;
+    private final double amount;
+
+    public Transaction(User from, User to, double amount) {
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+    }
+
+    public User getFrom() {
+        return from;
+    }
+
+    public User getTo() {
+        return to;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return from.getName() + " pays " + to.getName() + " : " + amount;
+    }
+}
